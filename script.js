57101e1ba0bc2e1900d17ea1c1b55aa0cf9cd144
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-04-13 12:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let text=document.querySelector("#text");
+let num=document.querySelector("#delay");
+let btn=document.querySelector("#btn");
+let output=document.querySelector("#output")
+
+
+btn.addEventListener("click",handleclick)
+
+  async function handleclick(){
+	       let resolve= await new Promise((resolve,reject)=>{
+			   setTimeout(()=>{
+				   resolve(text.value)
+			   },num.value)
+		   })
+	  output.innerText=resolve;
+	 
+	  text.value="";
+	  num.value="";
+
+	  
+}
+output.innerText=""
